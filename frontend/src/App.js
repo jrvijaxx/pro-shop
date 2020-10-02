54fbe6139dc437c8da08a1706df50c24925efae1
vijay saiwal <vijay.saiwal@procreator.in>
2020-10-02 22:20:55 +0530

    navbar setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          {" "}
+          <h1>Welcome to proshop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
