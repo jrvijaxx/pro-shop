06dc7dfe4f736dde905d0c4504854c9b6d868393
vijay saiwal <vijay.saiwal@procreator.in>
2020-10-03 23:40:26 +0530

    database connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
-const express = require("express");
-const dotenv = require('dotenv')
-const products = require("./data/products");
+import express from "express"
+import dotenv from 'dotenv'
+import colosr from 'colors'
+import connectDB from './config/db.js'
+import products from"./data/products.js"
 
 dotenv.config()
+
+connectDB()
 
 const app = express();
 
@@ -21,4 +25,4 @@ app.get("/api/products/:id", (req, res) => {
 
 const PORT = process.env.PORT || 5000
 
-app.listen(5000, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(5000, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
